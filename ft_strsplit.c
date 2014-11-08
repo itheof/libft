0f0e72e607d1c0aee8e305df09367c54a68defb0
itheof <theophile759@gmail.com>
2014-11-08 23:23:46 +0100

    Unbugged ft_strsplit

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: tvallee <tvallee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/05 14:51:18 by tvallee           #+#    #+#             */
-/*   Updated: 2014/11/08 21:49:44 by tvallee          ###   ########.fr       */
+/*   Updated: 2014/11/08 23:23:05 by tvallee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,8 +44,7 @@ char			**ft_strsplit(char const *s, char c)
 	if (!s)
 		return (NULL);
 	nb = ft_countstr(s, c);
-	r = (char **)malloc(sizeof(char) * (nb + 1));
-	ft_putnbr(nb);
+	r = malloc(sizeof(char **) * (nb + 1));
 	str.i = 0;
 	k = 0;
 	while ((size_t)k < nb)
@@ -56,12 +55,9 @@ char			**ft_strsplit(char const *s, char c)
 		while (s[str.j] != c)
 			str.j++;
 		r[k] = ft_strsub(s, str.i, str.j - str.i);
-		ft_putendl(r[k]);
 		k++;
 		str.i = str.j + 1;
 	}
 	r[k] = '\0';
-	ft_putnbr(k);
-	ft_putendl(r[1]);
 	return (r);
 }
